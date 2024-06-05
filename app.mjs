d37c6bc6529e8a115366586fe85fee053c2308ec
unknown <jithinbp007@gmail.com>
2024-06-05 22:45:49 +0530

    updating routes

--- app.mjs
+++ app.mjs
@@ -1,6 +1,9 @@
 import express from "express";
+import routes from "./routes/tasks.route.mjs";
 
 const app = express();
+
+app.use("/api/v1/tasks", routes);
 
 app.get("/login", (req, res) => {
   res.status(200).send("Login page");
