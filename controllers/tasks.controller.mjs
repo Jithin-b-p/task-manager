2eb702f8bc59dad1b2ed55612a6eb6d0dc7a1da2
unknown <jithinbp007@gmail.com>
2024-06-07 21:37:00 +0530

    adding an async wrapper middleware around the controller

--- controllers/tasks.controller.mjs
+++ controllers/tasks.controller.mjs
@@ -1,70 +1,42 @@
+import { asyncWrapper } from "../middleware/async.mjs";
 import Task from "../models/task.model.mjs";
 
-export const getAllTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).json({ tasks });
-  } catch (error) {
-    res.status(500).json({ errorType: error.name, msg: error.message });
-  }
-};
+export const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+});
 
-export const getTask = async (req, res) => {
+export const getTask = asyncWrapper(async (req, res) => {
   const { id } = req.params;
 
-  try {
-    const task = await Task.findById(id);
-    res.status(200).json({ task });
-  } catch (error) {
-    if (error.name === "CastError") {
-      return res.status(404).json({ msg: "id doesn't exist" });
-    }
-    res.status(500).json({ errorType: error.name, msg: error.message });
-  }
-};
+  const task = await Task.findById(id);
+  res.status(200).json({ task });
+});
 
-export const createTask = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-  } catch (error) {
-    res.status(500).json({ errorType: error.name, msg: error.message });
-  }
-};
+export const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
 
-export const updateTask = async (req, res) => {
+export const updateTask = asyncWrapper(async (req, res) => {
   const { id } = req.params;
 
-  try {
-    const task = await Task.findByIdAndUpdate(id, req.body, {
-      new: true,
-      runValidator: true,
-    });
-    if (!id) {
-      return res.status(404).json({ msg: "id doesn't exist" });
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    if (error.name === "CastError") {
-      return res.status(404).json({ msg: "id doesn't exist" });
-    }
-    res.status(500).json({ errorType: error.name, msg: error.message });
+  const task = await Task.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidator: true,
+  });
+  if (!id) {
+    return res.status(404).json({ msg: "id doesn't exist" });
   }
-};
+  res.status(200).json({ task });
+});
 
-export const deleteTask = async (req, res) => {
+export const deleteTask = asyncWrapper(async (req, res) => {
   const { id } = req.params;
 
-  try {
-    const task = await Task.findByIdAndDelete(id);
-    if (!task) {
-      return res.status(404).json({ msg: "id doesn't exist" });
-    }
-    res.status(200).json({ status: "success", deletedTask: task });
-  } catch (error) {
-    if (error.name === "CastError") {
-      return res.status(404).json({ msg: "id doesn't exist" });
-    }
-    res.status(500).json({ errorType: error.name, msg: error.message });
+  const task = await Task.findByIdAndDelete(id);
+  if (!task) {
+    return res.status(404).json({ msg: "id doesn't exist" });
   }
-};
+  res.status(200).json({ status: "success", deletedTask: task });
+});
