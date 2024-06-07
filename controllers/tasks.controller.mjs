1176e2b093276e5187d26311ff26509a54e6f77f
unknown <jithinbp007@gmail.com>
2024-06-07 12:36:39 +0530

    adding error handling for controller

--- controllers/tasks.controller.mjs
+++ controllers/tasks.controller.mjs
@@ -1,24 +1,54 @@
 import Task from "../models/task.model.mjs";
 
-export const getAllTasks = (req, res) => {
-  res.send("All tasks");
+export const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ errorType: error.name, msg: error.message });
+  }
 };
 
-export const getTask = (req, res) => {
+export const getTask = async (req, res) => {
   const { id } = req.params;
-  res.send(`A single task id: ${id}`);
+
+  try {
+    const task = await Task.findById(id);
+    res.status(200).json(task);
+  } catch (error) {
+    if (error.name === "CastError") {
+      return res.status(404).json({ msg: "id doesn't exist" });
+    }
+    res.status(500).json({ errorType: error.name, msg: error.message });
+  }
 };
 
 export const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-
-  res.status(201).json(task);
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ errorType: error.name, msg: error.message });
+  }
 };
 
 export const updateTask = (req, res) => {
   res.send("task updated");
 };
 
-export const deleteTask = (req, res) => {
-  res.send("task deleted");
+export const deleteTask = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const task = await Task.findByIdAndDelete(id);
+    if (!task) {
+      return res.status(404).json({ msg: "id doesn't exist" });
+    }
+    res.status(200).json({ status: "success", deletedTask: task });
+  } catch (error) {
+    if (error.name === "CastError") {
+      return res.status(404).json({ msg: "id doesn't exist" });
+    }
+    res.status(500).json({ errorType: error.name, msg: error.message });
+  }
 };
